1cdcaa1694efcaa76744cda889d1735ac0f5ad56
DalouhMoha <meddalouh00@gmail.com>
2024-07-31 16:11:43 +0100

    test

--- backend/src/main/java/com/backend/backend/BackendApplication.java
+++ backend/src/main/java/com/backend/backend/BackendApplication.java
@@ -3,11 +3,10 @@ package com.backend.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.backend")
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
-
 }
