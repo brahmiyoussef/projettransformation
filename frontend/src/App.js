237c99679f4d23f59ce8c4531388c6a56128934c
Brahmi Youssef (Stagiaire) <y.brahmi@attijariwafa.com>
2024-08-02 19:45:05 +0100

    modified app.js and other files to see correct result after merge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,19 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './Features/HomePage';
 import HistoryPage from './Features/HistoryPage';
-import 'C:\\Users\\s.gouait\\projects\\projettransformation\\frontend\\src\\App.css'; // Import the CSS file
-
+import 'C:\\Users\\y.brahmi\\Desktop\\projettranformation\\frontend\\src\\App.css';
+ // Import the CSS file
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/history" element={<HistoryPage />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+return (
+<Router>
+<div className="App">
+<Routes>
+<Route path="/" element={<HomePage />} />
+<Route path="/history" element={<HistoryPage />} />
+</Routes>
+</div>
+</Router>
+);
 }
-
 export default App;
+ 
---
+++ frontend/src/Features/DashboardPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Dashboard.mod.css'; // Importez les styles si nécessaire
+
+function DashboardPage() {
+  return (
+    <div style={{ display: 'flex' }}>
+      <div style={{ flex: 1, marginLeft: '80px', padding: '20px' }}>
+        {/* Contenu principal de la page */}
+        
+        <p>Bienvenue sur le tableau de bord!</p>
+      </div>
+    </div>
+  );
+}
+
+export default DashboardPage;
+ 
